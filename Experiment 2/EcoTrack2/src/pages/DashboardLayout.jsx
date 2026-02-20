a9a34b2e12de0cd0af9988c14c1eafc08d8e7655
MrRashid01 <rk.rashidkhanqwe@gmail.com>
2026-02-20 14:47:49 +0530

    exp

--- Experiment 2/EcoTrack2/src/pages/DashboardLayout.jsx
+++ Experiment 2/EcoTrack2/src/pages/DashboardLayout.jsx
@@ -9,6 +9,7 @@ const DashboardLayout = () => {
         <div style={{display:"flex", gap:"14px"}}>
           <Link to="summary" >Summary</Link>
           <Link to="analytics">Analytics</Link>
+          <Link to="logs">Logs</Link>
         </div>
       </nav>
 
--- Experiment 3/experiment3/src/main.jsx
+++ Experiment 3/experiment3/src/main.jsx
@@ -1,14 +1,17 @@
-// main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import store from "./store/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
   </StrictMode>
 );
