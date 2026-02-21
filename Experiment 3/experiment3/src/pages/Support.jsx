d6057eac6e03bc02ff510618387b12bfd181e32e
MrRashid01 <rk.rashidkhanqwe@gmail.com>
2026-02-21 12:44:56 +0530

    exp

--- Experiment 3/experiment3/src/pages/Support.jsx
+++ Experiment 3/experiment3/src/pages/Support.jsx
@@ -2,7 +2,7 @@ const Support = () => {
     return (
         <div>
             <h2>Welcome to the Technical</h2>
-            <p>Kindly Email us your query at - choudhary.priyanshu1401@gmail.com</p>
+            <p>Drop an email at - rk.rashidkhanqwe@gmail.com</p>
         </div>
     );
 };
